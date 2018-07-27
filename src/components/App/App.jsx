68f54ac3cf0ff700c5c7ca89f0d16763cea2e547
Alexandre Nicol <ninja@ninjabook.lan>
2018-07-27 10:54:44 +0100

    build

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,147 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
+        <p className="highlight">
+          <span className="k">const</span> <span className="n">me</span> = {'{'}
+          <br />
+          <span className="n">&nbsp; name:</span> <span className="l">'Alex Nicol'</span>,<br />
+          <span className="n">&nbsp; location:</span> <span className="l">'Brighton, UK'</span>,<br />
+          <span className="n">&nbsp; from:</span> <span className="l">'Brest, FR'</span>,<br />
+          <span className="n">&nbsp; skills:</span> [<span className="l">'Web'</span>, <span className="l">'Mobile'</span>, <span className="l">'Voice'</span>, <span className="l">'Chatbot'</span>],<br />
+          <span className="n">&nbsp; jobs:</span> {'{'}
+          <br />
+          <span className="n">&nbsp; &nbsp; current:</span> {'{'}
+          <br />
+          <span className="n">&nbsp; &nbsp; &nbsp; title:</span> <span className="l">'R&amp;D Digital Developer'</span>,<br />
+          <span className="n">&nbsp; &nbsp; &nbsp; at:</span> <span className="l">'EDF Energy R&amp;D UK Centre'</span>,<br />
+          <span className="n">&nbsp; &nbsp; &nbsp; since:</span> <span className="mi">1433116800</span>,<br />
+          <span className="n">&nbsp; &nbsp; </span> {'}'},<br />
+          <span className="n">&nbsp; &nbsp; previous:</span>{' '}
+          <span className="l">
+            '<a href="https://linkedin.com/in/alexnicol" target="_blank">
+              there
+            </a>'
+          </span>
+          ,
+          <br />
+          <span className="n">&nbsp; </span> {'}'},<br />
+          <span className="n">&nbsp; contact:</span>{' '}
+          <span className="l">
+            '<a href="mailto:alex@webnicol.fr">alex@webnicol.fr</a>'
+          </span>
+          ,
+          <br />
+          <span className="n">&nbsp; github:</span>
+          <span className="l">
+            '<a href="https://github.com/alexandrenicol" target="_blank">
+              alexandrenicol
+            </a>'
+          </span>
+          ,
+          <br />
+          <span className="n">&nbsp; twitter:</span>
+          <span className="l">
+            '<a href="https://twitter.com/nicol_alexandre" target="_blank">
+              @nicol_alexandre
+            </a>'
+          </span>
+          ,
+          <br />
+          <span className="n">&nbsp; articles:</span> {'['}
+          <br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://www.edfenergy.com/about/energy-innovation/innovation-blog/research-development-chatbots-lessons-learned" target="_blank">
+              Chatbots: 3 years on, what have we learnt?
+            </a>{' '}
+            - EDF Energy - 2018'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://www.edfenergy.com/about/energy-innovation/innovation-blog/research-development-chatbots" target="_blank">
+              Bringing chatbots to our business
+            </a>{' '}
+            - EDF Energy - 2018'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="hhttps://www.heartinternet.uk/blog/beginners-guide-chatbots-conversational-interfaces/" target="_blank">
+              A Beginner\'s Guide to Chatbots and Conversational Interfaces
+            </a>{' '}
+            - Heart Internet - 2017 - Published by{' '}
+            <a href="https://twitter.com/oliverlindberg" target="_blank">
+              @oliverlindberg
+            </a>'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://medium.com/@nicol_alexandre/dites-bonjour-aux-bots-f6afd4b68e2d" target="_blank">
+              Dites bonjour aux bots !
+            </a>{' '}
+            - 2016 (French)'
+          </span>,<br />
+          <span className="n">&nbsp; </span> {']'},<br />
+          <span className="n">&nbsp; talks:</span> {'['}
+          <br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://voice2018.webnicol.fr" target="_blank">
+              VUI Introduction
+            </a>{' '}
+            - Brest.js (Brest, Fr) - 2018'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://bot2018.webnicol.fr" target="_blank">
+              Chatbots 101
+            </a>{' '}
+            - Brighton Web Dev Meetup - 2018'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://www.meetup.com/London-Alexa-Devs/events/239181169/" target="_blank">
+              Get The Energy Talking
+            </a>{' '}
+            London Alexa Devs Meetup - 2017'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://riseofthebots.webnicol.fr/" target="_blank">
+              The Rise of The Bots
+            </a>{' '}
+            - Async (Brighton) - 2017'
+          </span>,<br />
+          <span className="n">&nbsp; </span> {']'},<br />
+          <span className="n">&nbsp; stuff:</span> {'['}
+          <br />
+          <span className="l">&nbsp; &nbsp; 'Parachute (soon)'</span>,<br />
+          <span className="l">&nbsp; &nbsp; 'Go AWS Lambda Router (soon)</span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://github.com/alexandrenicol/world-cup-playground" target="_blank">
+              World Cup Schedule playground (soon)
+            </a>'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://alexandrenicol.github.io/aws-lambda-router-wn/" target="_blank">
+              JavaScript AWS Lambda Router
+            </a>'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://www.amazon.co.uk/Alexandre-Nicol-Seagulls-Buses/dp/B01N2UF9BK" target="_blank">
+              Brighton &amp; Hove buses live times on Alexa Skill
+            </a>'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://assistant.google.com/u/2/services/a/uid/0000003daed71c62?hl=en&source=web" target="_blank">
+              2018 Resolutions on Google actions
+            </a>'
+          </span>,<br />
+          <span className="l">
+            &nbsp; &nbsp; '<a href="https://github.com/alexandrenicol/requemise" target="_blank">
+              Requemise
+            </a>'
+          </span>,<br />
+          <span className="n">&nbsp; </span> {']'},<br />
+          {'}'}
+          <br />
+          <br />
+          <span className="k2">console</span>.<span className="f">log</span>(<span className="n">me</span>);
+          <br />
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,3 +6,45 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const me = {
+  name: 'Alex Nicol',
+  location: 'Brighton, UK',
+  from: 'Brest, France',
+  skills: ['Web', 'Mobile', 'Voice', 'Chatbot'],
+  job: {
+    current: {
+      title: 'R&D Digital Developer',
+      at: 'EDF Energy R&D UK Centre',
+      since: 1433116800,
+    },
+    previous: 'there',
+  },
+  contact: 'alex@webnicol.fr',
+  github: 'alexandrenicol',
+  twitter: '@nicol_alexandre',
+  articles: [
+    'Chatbots: 3 years on, what have we learnt? - EDF Energy - 2018',
+    'Bringing chatbots to our business - EDF Energy - 2018',
+    'A Beginner\'s Guide to Chatbots and Conversational Interfaces - Heart Internet - 2017 - Published by @oliverlinberg',
+    'Dites bonjour aux bots ! - 2016 (French)',
+  ],
+  talks: [
+    'VUI Introduction - Brest.js (Brest, Fr) - 2018',
+    'Chatbots 101 - Brighton Web Dev Meetup - 2018',
+    'Get The Energy Talking - London Alexa Devs Meetup - 2017',
+    'The Rise of The Bots - Async (Brighton) - 2017',
+  ],
+  stuff: [
+    'Parachute (soon)',
+    'Go AWS Lambda Router (soon)',
+    'World Cup Schedule playground (soon)',
+    'JavaScript AWS Lambda Router',
+    'Brighton & Hove buses live times on Alexa Skill',
+    '2018 Resolutions on Google actions',
+    'Requemise',
+  ],
+};
+
+console.log('Hi There!');
+console.log(me);
